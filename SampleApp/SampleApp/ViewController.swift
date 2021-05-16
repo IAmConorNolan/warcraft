5ebe9ae186e396287886d2b66c5aeab7366744a0
Conor Nolan <IAmConorNolan@gmail.com>
2021-05-16 12:21:27 +0100

    Project Structure

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Conor Nolan on 16/05/2021.
+//
+
+import UIKit
+import Warcraft
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Warcraft/Warcraft/Warcraft.h
@@ -0,0 +1,18 @@
+//
+//  Warcraft.h
+//  Warcraft
+//
+//  Created by Conor Nolan on 16/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Warcraft.
+FOUNDATION_EXPORT double WarcraftVersionNumber;
+
+//! Project version string for Warcraft.
+FOUNDATION_EXPORT const unsigned char WarcraftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Warcraft/PublicHeader.h>
+
+
